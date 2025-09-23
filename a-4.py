aaec701d560cbf054ff16e04afd295f0c00379b2
yoshii01 <raichi441@gmail.com>
2025-09-23 23:10:02 +0900

    a-4回答

--- a-4.py
+++ a-4.py
@@ -2,5 +2,11 @@
     # 次のmembersというリストから "Bob" と "Tom" を取得して出力
     # 次のmembersというリストから "Bob" と "Tom" を取得して、それぞれ1件ずつ、"Bob" と "Tom"を出力
 
+from traceback import print_tb
+
+
 members = ["Bob", "Tom", "Ken"]
 
+# 回答
+print(members[0])
+print(members[1])
