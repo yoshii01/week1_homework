bf5f86e2a4d07b2c31812629aeee62717de4b666
yoshii01 <raichi441@gmail.com>
2025-09-24 00:04:13 +0900

    a-7回答

--- a-6.py
+++ a-6.py
@@ -6,3 +6,4 @@ odd_numbers = [1, 3, 5, 7, 9]
 # fodd_numbersというリストの各要素を順番に取り出
 for odd_number in odd_numbers:
     print(odd_number)
+#for 要素 in 繰り返し可能なオブジェクト「イテラブル」
--- a-7.py
+++ a-7.py
@@ -1,6 +1,8 @@
 # A-7: forによるループその2
     # for を使って even_numbers のそれぞれの値を2倍した値を出力
 
-
 even_numbers = [2, 4, 6, 8]
 
+#for 要素 in 繰り返し可能なオブジェクト「イテラブル」
+for even_number in even_numbers:
+    print(even_number* 2)
