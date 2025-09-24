f875cbe74a9e660dea2cb942bcbce624833feb4e
yoshii01 <raichi441@gmail.com>
2025-09-24 18:18:45 +0900

    a-9回答

--- a-9.py
+++ a-9.py
@@ -1,6 +1,10 @@
 # A-9: 辞書
     # 下記のコードが期待通り動作するような辞書を定義
 
+
+bob_info = {"first_name": "Bob", "family_name": "Dylan", "age": 79}
+
+
 print(bob_info["first_name"]) # "Bob"
 print(bob_info["family_name"]) # "Dylan"
 print(bob_info["age"]) # 79
