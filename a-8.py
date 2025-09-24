29e2bad53b074d49a73aedfa750ad53dd7b06501
yoshii01 <raichi441@gmail.com>
2025-09-24 18:38:47 +0900

    課題A完成

--- a-8.py
+++ a-8.py
@@ -11,9 +11,11 @@ from typing import ItemsView
 users_info = [["Bob", 79], ["Tom", 59], ["Ken", 61]]
 
 
-# ネスト構造からを並列構造に変換。それぞれnameとageに代入し出力
+# リストの形を横並びに変更し、nameとageに代入し出力
 for user in users_info:
     name = user[0]
     age = user[1]
+
+
     print(f"Name: {name}, Age: {age}")
 
