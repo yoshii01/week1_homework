3b869ab59678284d55be783aa76e0ccb45995c57
yoshii01 <raichi441@gmail.com>
2025-09-25 20:22:35 +0900

    課題B リスト作成

---
+++ python_basic2/b-4.py
@@ -0,0 +1,27 @@
+# 課題B-4: 天気情報の分析
+# 3都府県のいくつかの駅名とある日の最高気温のデータを辞書として持っています
+# このデータを使って3つの問を満たす実装をしてください
+
+# def main():
+#     # 3都府県のいくつかの駅名とある日の最高気温(単位: ℃)のデータを辞書として持っています
+#     weather_information = [
+#         {'prefecture': '東京都', 'station': '渋谷', 'temperature': 6.5},
+#         {'prefecture': '東京都', 'station': '池袋', 'temperature': 7.0},
+#         {'prefecture': '東京都', 'station': '新橋', 'temperature': 7.5},
+
+# efecture': '大阪府', 'station': '梅田', 'temperature': 8.2},
+#         {'prefecture': '大阪府', 'station': '大阪', 'temperature': 9.3},
+#         {'prefecture': '大阪府', 'station': '堺', 'temperature': 9.5},
+
+#         {'prefecture': '福岡県', 'station': '博多', 'temperature': 13.0},
+#         {'prefecture': '福岡県', 'station': '太宰府', 'temperature': 15.0},
+
+
+    # Q1. 全国の平均気温を計算してください(9.5となればOK)
+
+    # Q2. 大阪府のすべての駅名をカンマ区切りで出力してください( '梅田,大阪,堺' となればOK)
+
+    # Q3. 福岡県の平均気温を計算してください(14.0となればOK)
+
+
+# if __name__ == '__main__':
