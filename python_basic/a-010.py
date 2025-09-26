362f9561b4d49954dbbec1ba021136cc05c36427
yoshii01 <raichi441@gmail.com>
2025-09-26 19:15:38 +0900

    a-10 修正

--- python_basic/a-010.py
+++ python_basic/a-010.py
@@ -3,6 +3,24 @@
     #  ※ dice()関数は1から6の整数をランダムに返す
 
 
+# def 関数名1(引数1, 引数2, ...):
+#     # 何らかの処理を記述する
+#     return 結果
+# # 空行1行目
+# # 空行2行目
+# def 関数名2(引数1, 引数2, ...):
+#     # 何らかの処理を記述する
+#     return 結果
+
+import random
+
+
+def dice():
+    return random.randint(1, 6)
+
+
+print(dice())
+
 # import random
 
 # dice = [1, 2, 3, 4, 5, 6]
@@ -12,9 +30,9 @@
 # print(random.choice(dice)) # 1から6の整数をランダムに出力する
 
 
-import random
+# import random
 
-dice = [1, 2, 3, 4, 5, 6]
+# dice = [1, 2, 3, 4, 5, 6]
 
-idx = random.randint(1, 5)
-print(dice[idx]) 
+# idx = random.randint(1, 5)
+# print(dice[idx]) 
